159561e80f75004249ccbab46f6e80fc5602e293
alex <argriffi@ncsu.edu>
2013-11-15 17:44:14 -0500

    more connected components interface work

--- connectedcomponents.c
+++ connectedcomponents.c
@@ -16,9 +16,6 @@ typedef struct tagSUBGRAPH {
   // Points somewhere in ccgraph.compo_row_ptr.
   int *row_ptr;
 
-  // Points somewhere in ccgraph.compo_ind_ptr.
-  int *ind_ptr;
-
   // Points somewhere in ccgraph.local_to_global.
   // Maps local vertex indices to global vertex indices.
   int *local_to_global;
@@ -59,6 +56,67 @@ void ccgraph_compute(CCGRAPH *p,
   p->ncomponents = 0;
   p->nvertices = 0;
   p->nedges = 0;
+  p->compo_row_ptr[p->nvertices] = 0;
+}
+
+
+// This function is not specific to ccgraph.
+void compute_component(
+    const int *row_ptr, const int *col_ind,
+    int *parent_ws, int *deck_ws, int *next_ws,
+    int root, int label, int *component_labels,
+    int *vertices_out, int *nvertices_out
+    )
+{
+  // The component at the root should be unknown (-1).
+  // Set it to the current label.
+  assert(component_labels[root] == -1);
+  component_labels[root] = label;
+
+  // The root parent should be unknown (-1).
+  // Set it to -2 to indicate that it is the root.
+  assert(parent_ws[root] == -1);
+  parent_ws[root] = -2;
+
+  // The root vertex is on deck for breadth first search.
+  deck_ws[0] = root;
+  int ndeck = 1;
+
+  // Do the breadth first search.
+  // During the search update the local/global vertex index maps.
+  // After the search update the local csr graph.
+  while (ndeck)
+  {
+    int nnext = 0;
+    int ideck;
+    for (ideck=0; ideck<ndeck; ++ideck) {
+      int v = deck_ws[ideck];
+
+      // Update the map from the local to the global vertex index.
+      // Increment the vertex count to include the added vertex.
+      vertices_out[(*nvertices_out)++] = v;
+
+      for (i=row_ptr[v]; i<row_ptr[v+1]; ++i) {
+        int w = col_ind[i];
+        if (w != parent_ws[v]) {
+          if (parent_ws[w] >= 0) {
+            assert(component_labels[w] == label);
+          } else {
+            assert(component_labels[w] == -1);
+            component_labels[w] = label;
+            parent_ws[w] = v;
+            next_ws[nnext++] = w;
+          }
+        }
+      }
+    }
+
+    // Put the next array on deck.
+    int *tmp = deck_ws;
+    deck_ws = next_ws;
+    next_ws = tmp;
+    ndeck = nnext;
+  }
 }
 
 
@@ -83,73 +141,57 @@ void _ccgraph_compute_component(CCGRAPH *p,
   // The current label is the number of components in the ccgraph.
   int label = p->ncomponents;
 
-  // The component at the root should be unknown (-1).
-  // Set it to the current label.
-  assert(component_labels_out[root] == -1);
-  component_labels_out[root] = label;
-
-  // The root parent should be unknown (-1).
-  // Set it to -2 to indicate that it is the root.
-  assert(parent_ws[root] == -1);
-  parent_ws[root] = -2;
-
-  // Pick the subgraph relevant to this component.
+  // Pick the subgraph that corresponds to this component.
   SUBGRAPH *subgraph = p->subgraph[p->ncomponents];
 
   // Set up the subgraph indirection relative to the ccgraph object.
   subgraph->local_to_global = p->local_to_global + p->nvertices;
   subgraph->row_ptr = p->compo_row_ptr + p->nvertices;
-  subgraph->col_ind = p->compo_col_ind + p->nedges;
 
-  // Update the maps between local and global vertex indices.
-  subgraph->local_to_global[subgraph->nvertices] = root;
-  p->global_to_local[root] = subgraph->nvertices;
+  // Initialize the subgraph to have no vertices.
+  subgraph->nvertices = 0;
 
-  // Increment vertex counts to include the root vertex.
-  p->nvertices++;
-  subgraph->nvertices++;
+  // Compute the connected component.
+  compute_component(row_ptr, col_ind,
+      parent_ws, deck_ws, next_ws,
+      root, label, component_labels,
+      subgraph->local_to_global, &subgraph->nvertices)
 
-  // The root vertex is on deck for breadth first search.
-  deck_ws[0] = r;
-  int ndeck = 1;
+  // Do some simple post-processing for each vertex in the component.
+  for (v_local=0; v_local<subgraph->nvertices; ++v_local) {
 
-  // Do the breadth first search.
-  int depth = 0;
-  while (ndeck)
-  {
-    int nnext = 0;
-    int ideck;
-    for (ideck=0; ideck<ndeck; ++ideck) {
-      int v = deck_ws[ideck];
-      for (i=row_ptr[v]; i<row_ptr[v+1]; ++i) {
-        int w = col_ind[i];
-        if (w != parent_ws[v]) {
+    // Get the global index of the vertex.
+    v_global = subgraph->local_to_global[v_local];
 
-          // If we have already reached this node earlier
-          // (possible if the graph has cycles) then skip the vertex.
-          // Otherwise if we have not reached the vertex earlier
-          // then set the label and the parent and add it to the shell,
-          // and also add it to the list of vertices in the component,
-          // and increment the number of vertices in the component.
-          if (parent_ws[w] >= 0) {
-            assert(component_labels_out[w] == label_in);
-          } else {
-            assert(component_labels_out[w] == -1);
-            component_labels_out[w] = label_in;
-            vertices_out[(*nvertices_out)++] = w;
-            parent_ws[w] = v;
-            next_ws[nnext++] = w;
-          }
-        }
-      }
+    // Reset the vertex parent.
+    parent_ws[v_global] = -1;
+
+    // Fill the global to local map.
+    p->global_to_local[v_global] = v_local;
+  }
+
+  // Update the subgraph csr.
+  for (v_local=0; v_local<subgraph->nvertices; ++v_local) {
+
+    // Get the global index of the vertex.
+    v_global = subgraph->local_to_global[v_local];
+
+    // For each edge, add the local sink vertex to the compo col ind array.
+    for (i=row_ptr[v_global]; i<row_ptr[v_global+1]; ++i) {
+      int w_global = col_ind[i];
+      int w_local = p->global_to_local[w_global];
+      p->compo_col_ind[p->nedges++] = w_local;
     }
 
-    // Put the next array on deck.
-    int *tmp = deck_ws;
-    deck_ws = next_ws;
-    next_ws = tmp;
-    ndeck = nnext;
+    // Add to the compo row ptr list.
+    p->compo_row_ptr[p->nvertices + 1] = p->nedges;
+
+    // Increment the number of vertices for which the edges are tracked.
+    p->nvertices++;
   }
+
+  // Increment the number of components for which the local graph is available.
+  p->ncomponents++;
 }
 
 
