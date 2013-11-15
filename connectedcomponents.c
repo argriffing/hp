9d7cc752cf08a94178674b230a080a695240a172
alex <argriffi@ncsu.edu>
2013-11-15 16:23:43 -0500

    more work on connected components interface

--- connectedcomponents.c
+++ connectedcomponents.c
@@ -6,7 +6,9 @@
 // Connected components in csr format.
 
 
-typedef struct tagLOCALGRAPH {
+// The idea of this structure is to avoid malloc/free in an inner loop.
+// This structure should not own the memory referenced by any of its pointers.
+typedef struct tagSUBGRAPH {
 
   // This is the number of local vertices.
   int nvertices;
@@ -21,23 +23,148 @@ typedef struct tagLOCALGRAPH {
   // Maps local vertex indices to global vertex indices.
   int *local_to_global;
 
-} LOCALGRAPH;
+} SUBGRAPH;
 
 
 typedef struct tagCCGRAPH {
   int nvertices;
   int ncomponents;
-  LOCALGRAPH *subgraphs;
+  int nedges;
+  LOCALGRAPH *subgraph; // length is nvertices (as upper bound on ncomponents)
   int *global_to_local; // length is nvertices
   int *local_to_global; // length is nvertices
   int *compo_row_ptr; // length is nvertices + 1
   int *compo_ind_ptr; // length is nedges
 } CCGRAPH;
 
-void ccgraph_init(CCGRAPH *p,
+
+// This function needs to be called only once per application,
+// if the max number of vertices and max number of edges is known.
+// You do not need to call this function every time you want
+// to compute the connected components.
+void ccgraph_init(CCGRAPH *p, int max_nvertices, int max_nedges)
+{
+  p->subgraph = (LOCALGRAPH *) malloc(max_nvertices * sizeof(CCGRAPH));
+  p->global_to_local = (int *) malloc(max_nvertices * sizeof(int));
+  p->local_to_global = (int *) malloc(max_nvertices * sizeof(int));
+  p->compo_row_ptr = (int *) malloc((max_nvertices + 1) * sizeof(int));
+  p->compo_ind_ptr = (int *) malloc(max_nedges * sizeof(int));
+}
+
+
+// This function assumes that the ccgraph has already been initialized.
+void ccgraph_compute(CCGRAPH *p,
     const int *row_ptr, const int *col_ind, int nvertices)
 {
+  p->ncomponents = 0;
+  p->nvertices = 0;
+  p->nedges = 0;
 }
+
+
+// This is a helper function for flood filling a single component.
+// The function name underscore prefix indicates that this function is not
+// part of the public interface of this module.
+// C may have keywords to enforce this somehow but I'm not doing that.
+//
+// The parent vertices are assumed to have been set to -1,
+// and all vertices in the connected component of the root are assumed to be -1
+// representing membership in a currently unknown component.
+// The set of vertices in the component is also tracked.
+// The local to global map and the global to local map should each
+// have -1 entries to represent unknown values.
+//
+void _ccgraph_compute_component(CCGRAPH *p,
+    const int *row_ptr, const int *col_ind,
+    int *parent_ws, int *deck_ws, int *next_ws,
+    int root, int *component_labels
+    )
+{
+  // The current label is the number of components in the ccgraph.
+  int label = p->ncomponents;
+
+  // The component at the root should be unknown (-1).
+  // Set it to the current label.
+  assert(component_labels_out[root] == -1);
+  component_labels_out[root] = label;
+
+  // The root parent should be unknown (-1).
+  // Set it to -2 to indicate that it is the root.
+  assert(parent_ws[root] == -1);
+  parent_ws[root] = -2;
+
+  // Pick the subgraph relevant to this component.
+  SUBGRAPH *subgraph = p->subgraph[p->ncomponents];
+
+  // Set up the subgraph indirection relative to the ccgraph object.
+  subgraph->local_to_global = p->local_to_global + p->nvertices;
+  subgraph->row_ptr = p->compo_row_ptr + p->nvertices;
+  subgraph->col_ind = p->compo_col_ind + p->nedges;
+
+  // Update the maps between local and global vertex indices.
+  subgraph->local_to_global[subgraph->nvertices] = root;
+  p->global_to_local[root] = subgraph->nvertices;
+
+  // Increment vertex counts to include the root vertex.
+  p->nvertices++;
+  subgraph->nvertices++;
+
+  // The root vertex is on deck for breadth first search.
+  deck_ws[0] = r;
+  int ndeck = 1;
+
+  // Do the breadth first search.
+  int depth = 0;
+  while (ndeck)
+  {
+    int nnext = 0;
+    int ideck;
+    for (ideck=0; ideck<ndeck; ++ideck) {
+      int v = deck_ws[ideck];
+      for (i=row_ptr[v]; i<row_ptr[v+1]; ++i) {
+        int w = col_ind[i];
+        if (w != parent_ws[v]) {
+
+          // If we have already reached this node earlier
+          // (possible if the graph has cycles) then skip the vertex.
+          // Otherwise if we have not reached the vertex earlier
+          // then set the label and the parent and add it to the shell,
+          // and also add it to the list of vertices in the component,
+          // and increment the number of vertices in the component.
+          if (parent_ws[w] >= 0) {
+            assert(component_labels_out[w] == label_in);
+          } else {
+            assert(component_labels_out[w] == -1);
+            component_labels_out[w] = label_in;
+            vertices_out[(*nvertices_out)++] = w;
+            parent_ws[w] = v;
+            next_ws[nnext++] = w;
+          }
+        }
+      }
+    }
+
+    // Put the next array on deck.
+    int *tmp = deck_ws;
+    deck_ws = next_ws;
+    next_ws = tmp;
+    ndeck = nnext;
+  }
+}
+
+
+void ccgraph_destroy(CCGRAPH *p)
+{
+  free(p->subgraph);
+  free(p->global_to_local);
+  free(p->local_to_global);
+  free(p->compo_row_ptr);
+  free(p->compo_ind_ptr);
+}
+
+
+
+
 
 // Get the connected component associated with a single vertex.
 // This is a helper for the function that gets all connected
