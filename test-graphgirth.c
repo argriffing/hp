1ae9037b7be561b62163681c529552ab10111a60
alex <argriffi@ncsu.edu>
2013-11-18 19:31:12 -0500

    more tests of graph girth

--- test-graphgirth.c
+++ test-graphgirth.c
@@ -4,11 +4,151 @@
 #include "sparsetools.h"
 #include "graphgirth.h"
 
+int test_small_cycle_1()
+{
+  // Check if we can find the small cycle
+  // in a graph with multiple overlapping cycles.
+  //
+  //   0 ------- 1 ------- 2 ------- 3
+  //   |                             |
+  //   4 -- 5 -- 6 -- 7 -- 8 -- 9 -- 10
+  //   |                             |
+  //  11 ------ 12 ------- 13 ------ 14
+  //
+  int lil[] = {
+    0, 1,
+    1, 2,
+    2, 3,
+    4, 5,
+    5, 6,
+    6, 7,
+    7, 8,
+    8, 9,
+    9, 10,
+    11, 12,
+    12, 13,
+    13, 14,
+    0, 4,
+    4, 11,
+    3, 10,
+    10, 14,
+  };
+  int nvertices = 15;
+  int nedges = 16;
+  int girth = 10;
+
+  // Allocate csr memory and convert to csr format.
+  int *row_ptr = (int *) malloc((nvertices + 1) * sizeof(int));
+  int *col_ind = (int *) malloc(2 * nedges * sizeof(int));
+  lil_to_csr(nvertices, nedges, lil, row_ptr, col_ind);
+
+  // Allocate memory for the search for the smallest cycle.
+  BFS_WS bfs_ws;
+  bfs_ws_init(&bfs_ws, nvertices);
+  int *depth_ws = (int *) malloc(nvertices * sizeof(int));
+
+  // Compute girth.
+  int computed_girth = get_girth(row_ptr, col_ind, nvertices,
+      &bfs_ws, depth_ws);
+
+  // Check the girth.
+  int failflag = 0;
+  if (computed_girth != girth) {
+    printf("failed to compute the girth correctly\n");
+    printf("expected girth: %d\n", girth);
+    printf("computed girth: %d\n", computed_girth);
+    printf("\n");
+    failflag = 1;
+  }
+
+  // Free some memory.
+  bfs_ws_destroy(&bfs_ws);
+  free(depth_ws);
+  free(row_ptr);
+  free(col_ind);
+
+  return failflag;
+}
+
+int test_small_cycle_0()
+{
+  // Check if we can find the small cycle in a hollow-barbell graph.
+  //
+  //   0--1
+  //   |  |
+  //   3--2
+  //   |
+  //   4
+  //   |
+  //   5
+  //   |
+  //   6--7
+  //   |  |
+  //  11  8
+  //   |  |
+  //  10--9
+  //
+  int lil[] = {
+    0, 1,
+    1, 2,
+    2, 3,
+    3, 0,
+    3, 4,
+    4, 5,
+    5, 6,
+    6, 7,
+    7, 8,
+    8, 9,
+    9, 10,
+    10, 11,
+    11, 6,
+  };
+  int nvertices = 12;
+  int nedges = 13;
+  int girth = 4;
+
+  // Allocate csr memory and convert to csr format.
+  int *row_ptr = (int *) malloc((nvertices + 1) * sizeof(int));
+  int *col_ind = (int *) malloc(2 * nedges * sizeof(int));
+  lil_to_csr(nvertices, nedges, lil, row_ptr, col_ind);
+
+  // Allocate memory for the search for the smallest cycle.
+  BFS_WS bfs_ws;
+  bfs_ws_init(&bfs_ws, nvertices);
+  int *depth_ws = (int *) malloc(nvertices * sizeof(int));
+
+  // Compute girth.
+  int computed_girth = get_girth(row_ptr, col_ind, nvertices,
+      &bfs_ws, depth_ws);
+
+  // Check the girth.
+  int failflag = 0;
+  if (computed_girth != girth) {
+    printf("failed to compute the girth correctly\n");
+    printf("expected girth: %d\n", girth);
+    printf("computed girth: %d\n", computed_girth);
+    printf("\n");
+    failflag = 1;
+  }
+
+  // Free some memory.
+  bfs_ws_destroy(&bfs_ws);
+  free(depth_ws);
+  free(row_ptr);
+  free(col_ind);
+
+  return failflag;
+}
+
+
+
+
 int t0()
 {
   // This graph is connected.
   // It is like a theta graph with a tree attached onto it.
-  // The smallest cycle of this graph is {7, 8, 10, 9}.
+  // The smallest cycle of this graph is {7, 8, 10, 9},
+  // so the girth is 4.
   // If you root at early nodes like < 5 then you will
   // not find that cycle using a topo sort detection.
   int lil[] = {
@@ -29,41 +169,44 @@ int t0()
   };
   int nvertices = 13;
   int nedges = 14;
+  int girth = 4;
 
   // Allocate csr memory and convert to csr format.
   int *row_ptr = (int *) malloc((nvertices + 1) * sizeof(int));
   int *col_ind = (int *) malloc(2 * nedges * sizeof(int));
   lil_to_csr(nvertices, nedges, lil, row_ptr, col_ind);
 
-  int root = 3;
-
   // Allocate memory for the search for the smallest cycle.
   BFS_WS bfs_ws;
   bfs_ws_init(&bfs_ws, nvertices);
   int *depth_ws = (int *) malloc(nvertices * sizeof(int));
 
+  // Smoke test for girth upper bound.
+  int root = 3;
+  int computed_girth_ub = get_girth_ub(row_ptr, col_ind, nvertices, root,
+      &bfs_ws, depth_ws);
+
   // Compute girth.
-  int girth_ub = get_girth_ub(row_ptr, col_ind, nvertices, root,
-      &bfs_ws, depth_ws);
-  int girth = get_girth(row_ptr, col_ind, nvertices,
+  int computed_girth = get_girth(row_ptr, col_ind, nvertices,
       &bfs_ws, depth_ws);
 
-  printf("root: %d\n", root);
-  printf("girth upper bound: %d\n", girth_ub);
-  printf("\n");
+  // Check the girth.
+  int failflag = 0;
+  if (computed_girth != girth) {
+    printf("failed to compute the girth correctly\n");
+    printf("expected girth: %d\n", girth);
+    printf("computed girth: %d\n", computed_girth);
+    printf("\n");
+    failflag = 1;
+  }
 
-  printf("girth: %d\n", girth);
-  printf("\n");
-  
-  // Free girth calculation memory.
+  // Free some memory.
   bfs_ws_destroy(&bfs_ws);
   free(depth_ws);
-
-  // Free csr memory.
   free(row_ptr);
   free(col_ind);
 
-  return 1;
+  return failflag;
 }
 
 
@@ -72,6 +215,8 @@ int main()
   int nfails = 0;
 
   nfails += t0();
+  nfails += test_small_cycle_0();
+  nfails += test_small_cycle_1();
 
   if (nfails) {
     printf("failed testing: %d tests failed\n", nfails);
