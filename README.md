fe3ce44915d616b6f48f7fe3ac404b9baa2a89f9
alex <argriffi@ncsu.edu>
2013-11-20 13:49:30 -0500

    input the walk using stdin instead of a command line parameter

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ```
 $ gcc -o solve-brglez-plan-b solve-brglez-plan-b.c sparsetools.c subsetsum.c graphgirth.c connectedcomponents.c breadthfirst.c bondsubgraph.c
-$ ./solve-brglez-plan-b 10 dlluruluurdrdrurddl
+$ echo dlluruluurdrdrurddl | ./solve-brglez-plan-b 10
 9
 10100110100101100101
 ```
--- solve-brglez-plan-b.c
+++ solve-brglez-plan-b.c
@@ -102,31 +102,56 @@ int solve_potential_bond_graph(
 // lurulurulldlulddrdddruuu
 int main(int argc, char *argv[])
 {
-  if (argc != 3) {
+  if (argc != 2) {
     printf("Usage:\n");
-    printf("%s 10 dlluruluurdrdrurddl\n", argv[0]);
+    printf("%s 10\n", argv[0]);
     return 1;
   }
   int i;
   char c;
 
   int k = atoi(argv[1]);
-  int path_length = strlen(argv[2]);
-
-  // Check the input before we get too far.
-
   if (k < 2) {
     printf("Error: the sequence weight should be at least 2\n");
     return 1;
   }
 
-  if (path_length < 3) {
+  // Allocate room for the walk to be read from stdin.
+  int max_walk_len = 1000000;
+  int max_walk_size = max_walk_len + 1;
+  char *walk = (char *) calloc(max_walk_size, sizeof(char));
+
+  // Read the walk from stdin.
+  fgets(walk, max_walk_size, stdin);
+  int raw_walk_length = strlen(walk);
+
+  // Check that the input is not too big.
+  if (raw_walk_length == max_walk_len) {
+    printf("Error: please use a walk input less than %d bytes\n", max_walk_len);
+    return 1;
+  }
+
+  // Remove whitespace.
+  char *pfront = walk;
+  char *pback = walk;
+  while (*pback) {
+    c = *(pback++);
+    if (!isspace(c)) {
+      *(pfront++) = c;
+    }
+  }
+  *pfront = 0;
+  int walk_length = strlen(walk);
+
+  // Check the input before we get too far.
+
+  if (walk_length < 3) {
     printf("Error: the walk should take at least 3 steps\n");
     return 1;
   }
 
-  for (i=0; i<path_length; ++i) {
-    c = argv[2][i];
+  for (i=0; i<walk_length; ++i) {
+    c = walk[i];
     if (c != 'l' && c != 'r' && c != 'u' && c != 'd') {
       printf("Error: each step of the walk must be one of {l, r, u, d}\n");
       return 1;
@@ -135,9 +160,18 @@ int main(int argc, char *argv[])
 
   // Define the number of vertices in the potential bond graph,
   // and an upper bound on the number of edges in the potential bond graph.
-  int nvertices = path_length + 1;
+  int nvertices = walk_length + 1;
   int nedges_max_undirected = nvertices + 1;
   int nedges_max_directed = 2 * nedges_max_undirected;
+
+  // If the requested weight is larger than the number of vertices,
+  // then this is a problem.
+  if (k > nvertices) {
+    printf("Error: the requested sequence weight ");
+    printf("is greater than the sequence length");
+    free(walk);
+    return 1;
+  }
 
   // Allocate room for the csr graph.
   int *row_ptr = (int *) malloc((nvertices + 1) * sizeof(int));
@@ -151,22 +185,29 @@ int main(int argc, char *argv[])
   x[0] = 0;
   y[0] = 0;
   int step;
-  for (step=0; step<path_length; ++step) {
+  for (step=0; step<walk_length; ++step) {
     x[step + 1] = x[step];
     y[step + 1] = y[step];
-    c = argv[2][step];
+    c = walk[step];
     if (c == 'l') x[step + 1]--;
     if (c == 'r') x[step + 1]++;
     if (c == 'u') y[step + 1]--;
     if (c == 'd') y[step + 1]++;
   }
 
+  // Free the walk memory.
+  free(walk);
+
   // Initialize the csr graph of potential bonds.
   row_ptr[0] = 0;
   int j;
+  int ncollisions = 0;
   for (i=0; i<nvertices; ++i) {
     row_ptr[i+1] = row_ptr[i];
     for (j=0; j<nvertices; ++j) {
+      if (i != j && x[i] == x[j] && y[i] == y[j]) {
+        ncollisions++;
+      }
       if (abs(j-i) > 1) {
         if (abs(x[j] - x[i]) + abs(y[j] - y[i]) == 1) {
           col_ind[row_ptr[i+1]++] = j;
@@ -178,6 +219,13 @@ int main(int argc, char *argv[])
   // Free the coordinates per vertex.
   free(x);
   free(y);
+
+  if (ncollisions) {
+    printf("Error: the conformation includes self-intersections\n");
+    free(row_ptr);
+    free(col_ind);
+    return 1;
+  }
 
   // Solve a densest k-subgraph problem on the potential bond graph.
   int failflag = solve_potential_bond_graph(row_ptr, col_ind, nvertices, k);
