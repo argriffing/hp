cf1e9f7e25bc8beb58dbc50157f15e3d5a9ed102
alex <argriffi@ncsu.edu>
2013-11-21 18:40:50 -0500

    add a new grid module

---
+++ plancgrid.c
@@ -0,0 +1,38 @@
+#include "plancgrid.h"
+
+void grid_init(GRID *p, int n)
+{
+  p->n = n;
+  p->radius = n;
+  p->nrows = 2*p->radius + 1;
+  p->ncols = 2*p->radius + 1;
+  p->area = p->nrows * p->ncols;
+  p->origin_row = p->radius;
+  p->origin_col = p->radius;
+  p->data = (int *) malloc(p->area * sizeof(int));
+  int i;
+  for (i=0; i<p->area; ++i) {
+    p->data[i] = -1;
+  }
+}
+
+void grid_destroy(GRID *p)
+{
+  free(p->data);
+}
+
+int grid_get(GRID *p, int row, int col)
+{
+  return p->data[row * p->ncols + col];
+}
+
+void grid_set(GRID *p, int row, int col, int value)
+{
+  p->data[row * p->ncols + col] = value;
+}
+
+void grid_clear(GRID *p, int row, int col)
+{
+  grid_set(p, row, col, -1);
+}
+
---
+++ plancgrid.h
@@ -0,0 +1,45 @@
+/*
+ * This module is for the grid for the "plan c" solver.
+ *
+ * The idea is to allocate a 2d grid of integers to track the growing sequence.
+ * The first site in the sequence is anchored to the center of the grid,
+ * and the size of the allocated grid is large enough so that its
+ * radius is greater than the max sequence length.
+ *
+ * The grid is initially filled with values of -1 indicating lack of presence
+ * of a sequence site at the location.
+ * As the sequence grows, the value of the grid at sequence site j
+ * will have the integer value j.
+ * During backtracking, the retracting sequence will refill its formerly
+ * occupied grid sites with -1 indicating that the sequence no longer
+ * occupies those positions on the grid.
+ *
+ */
+
+#ifndef PLANCGRID_HEADER
+#define PLANCGRID_HEADER
+
+typedef struct tag_GRID
+{
+  int n;
+  int radius;
+  int origin_row;
+  int origin_col;
+  int nrows;
+  int ncols;
+  int area;
+  int *data;
+} GRID;
+
+void grid_init(GRID *p, int n);
+
+void grid_destroy(GRID *p);
+
+int grid_get(GRID *p, int row, int col);
+
+void grid_set(GRID *p, int row, int col, int value);
+
+void grid_clear(GRID *p, int row, int col);
+
+#endif
+
